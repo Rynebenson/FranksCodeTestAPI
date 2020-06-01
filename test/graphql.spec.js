af90596a79d002132d6b8bcc9ad7d0d19c673efc
rynebenson <rynebensont@gmail.com>
2020-06-01 13:12:14 -0400

    added tests

--- test/graphql.spec.js
+++ test/graphql.spec.js
@@ -1,10 +1,59 @@
-const server = require("../index")
-const { createTestClient } = require("apollo-server-testing")
+const app = require("../index")
+const request = require("supertest")
+const chai = require("chai")
 
-const { query } = createTestClient(server)
+const expect = chai.expect
+const should = chai.should()
 
-it('Fetch Special By Zip', async () => {
-    const res = await query({ query: GET_SPECIALS, variables: { filter: "91001" } })
+describe('GraphQL', () => {
+    it('Fetch Specials In Service Area 91001', done => {
+        request("http://localhost:3001/graphql")
+            .post('/graphql')
+            .send({ query: '{ specials(filter:"91001") { _id cheese { _id name country price } zip percent_discount out_of_stock  } }' })
+            .expect(200)
+            .end((err, res) => {
+                if(err) return done(err)
 
-    expect(res).toMatchSnapshot();
+                res.body.data.specials[0].should.have.property('_id')
+                res.body.data.specials[0].should.have.property('cheese')
+                res.body.data.specials[0].should.have.property('zip')
+                res.body.data.specials[0].should.have.property('percent_discount')
+                res.body.data.specials[0].should.have.property('out_of_stock')
+                done()
+            })
+    })
+
+    it('Fetch Specials By Filtering Country and Zip', done => {
+        request("http://localhost:3001/graphql")
+            .post('/graphql')
+            .send({ query: '{ specials(filter: "italy", zip:"91001") { _id cheese { _id name country price } zip percent_discount out_of_stock  } }' })
+            .expect(200)
+            .end((err, res) => {
+                if(err) return done(err)
+
+                res.body.data.specials[0].should.have.property('_id')
+                res.body.data.specials[0].should.have.property('cheese')
+                res.body.data.specials[0].should.have.property('zip')
+                res.body.data.specials[0].should.have.property('percent_discount')
+                res.body.data.specials[0].should.have.property('out_of_stock')
+                done()
+            })
+    })
+
+    it('Fetch Specials By Filtering Name and Zip', done => {
+        request("http://localhost:3001/graphql")
+            .post('/graphql')
+            .send({ query: '{ specials(filter: "fontina", zip:"91001") { _id cheese { _id name country price } zip percent_discount out_of_stock  } }' })
+            .expect(200)
+            .end((err, res) => {
+                if(err) return done(err)
+
+                res.body.data.specials[0].should.have.property('_id')
+                res.body.data.specials[0].should.have.property('cheese')
+                res.body.data.specials[0].should.have.property('zip')
+                res.body.data.specials[0].should.have.property('percent_discount')
+                res.body.data.specials[0].should.have.property('out_of_stock')
+                done()
+            })
+    })
 })
